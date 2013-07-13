41b8803fac38ebd07953df3be29d4d209508d469
Nick Ragaz <nick.ragaz@gmail.com>
2013-07-13 14:09:31 -0400

    Comment out Railtie
    Including SendGrid in ActionMailer::Base makes its `mail` method inaccessible to subclasses.

--- lib/sendgrid/railtie.rb
+++ lib/sendgrid/railtie.rb
@@ -1,11 +1,13 @@
+=begin
 require 'rails'
 
 module SendGrid
   class Railtie < Rails::Railtie # :nodoc:
 
-    initializer "send_grid.action_mailer" do
-      ActiveSupport.on_load(:action_mailer) { ActionMailer::Base.send(:include, SendGrid) }
-    end
+    # initializer "send_grid.action_mailer" do
+    #   ActiveSupport.on_load(:action_mailer) { ActionMailer::Base.send(:include, SendGrid) }
+    # end
 
   end
 end
+=end
